99d0b25633d59d9133c6d5c119b88d32ad4d2bcf
Si Thu <sithu.mdy.07@gmail.com>
2018-09-29 18:42:42 +0630

    Add mongoose update and Add Production File

---
+++ config/env/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db: 'mongodb://localhost:27017/starter',
+    jwt_secret: 'pr@ducti@n'
+};
--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,8 @@ const config = require('./config');
 const mongoose = require('mongoose');
 
 module.exports = function () {
+    // Use for future version
+    mongoose.set('useCreateIndex', true);
     const db = mongoose.connect(config.db, { useNewUrlParser: true });
 
     // Models
